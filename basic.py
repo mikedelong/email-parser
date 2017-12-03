52e680312e5647570e03c87e28f31d649e4bde7b
Mike DeLong <mikedelong@outlook.com>
2017-12-03 17:50:44 -0500

    feature: initial logging and overall elapsed time.

--- basic.py
+++ basic.py
@@ -1,2 +1,20 @@
 
 import pyzmail
+
+import logging
+import time
+
+start_time = time.time()
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+elapsed_time = time.time() - start_time
+logger.debug('elapsed time %d seconds', elapsed_time)
