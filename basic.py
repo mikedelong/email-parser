dc58e2ad6ab99dd58151c6379d7acd6a0ccf9a25
Mike DeLong <mikedelong@outlook.com>
2017-12-03 17:59:41 -0500

    bugfix: added missing import sys.

--- basic.py
+++ basic.py
@@ -3,6 +3,7 @@ import pyzmail
 
 import logging
 import time
+import sys
 
 # http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
 reload(sys)
