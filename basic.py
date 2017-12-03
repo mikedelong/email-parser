d70f1783f12783c8cf1aa967292773ffc813aab9
Mike DeLong <mikedelong@outlook.com>
2017-12-03 17:52:32 -0500

    feature: fix for default encoding.

--- basic.py
+++ basic.py
@@ -4,6 +4,10 @@ import pyzmail
 import logging
 import time
 
+# http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
+reload(sys)
+sys.setdefaultencoding("utf8")
+
 start_time = time.time()
 
 # set up logging
