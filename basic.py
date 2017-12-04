24ba8522983ec4a1a2fa5499e32ebdc65885579b
Mike DeLong <mikedelong@outlook.com>
2017-12-04 18:50:16 -0500

    cleanup: removed dead code from pyzmail version.

--- basic.py
+++ basic.py
@@ -1,14 +1,7 @@
-# import pyzmail
-
 import logging
 import time
-# import sys
 import win32com.client
 import os
-
-# http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
-# reload(sys)
-# sys.setdefaultencoding("utf8")
 
 start_time = time.time()
 
@@ -23,8 +16,6 @@ console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
 current_file = '.\example.msg'
-# with open(current_file, 'rb') as fp:
-#     message = pyzmail.message_from_file(fp)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 logger.debug(dir_path)
