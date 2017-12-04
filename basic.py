a3367bbeffaecc9a940c932e9d1ab31b8e3c4e2f
Mike DeLong <mikedelong@outlook.com>
2017-12-04 18:49:01 -0500

    feature: a first working version that runs on Windows; we will need to rethink this for use on e.g. Linux.

--- basic.py
+++ basic.py
@@ -1,12 +1,14 @@
-import pyzmail
+# import pyzmail
 
 import logging
 import time
-import sys
+# import sys
+import win32com.client
+import os
 
 # http://mypy.pythonblogs.com/12_mypy/archive/1253_workaround_for_python_bug_ascii_codec_cant_encode_character_uxa0_in_position_111_ordinal_not_in_range128.html
-reload(sys)
-sys.setdefaultencoding("utf8")
+# reload(sys)
+# sys.setdefaultencoding("utf8")
 
 start_time = time.time()
 
@@ -20,10 +22,15 @@ logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
-current_file = './example.msg'
-with open(current_file, 'rb') as fp:
-    message = pyzmail.message_from_file(fp)
-    
+current_file = '.\example.msg'
+# with open(current_file, 'rb') as fp:
+#     message = pyzmail.message_from_file(fp)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+logger.debug(dir_path)
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+msg = outlook.OpenSharedItem(dir_path + r"/example.msg")
 
 elapsed_time = time.time() - start_time
 logger.debug('elapsed time %d seconds', elapsed_time)
