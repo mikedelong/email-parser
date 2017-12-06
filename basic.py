096feca183b06910a1d1f1ddcc914ba4550fa290
Mike DeLong <mikedelong@outlook.com>
2017-12-05 20:37:44 -0500

    feature: now we process all the .msg files in the current folder.

--- basic.py
+++ basic.py
@@ -16,25 +16,31 @@ console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-current_file = dir_path + r"/example.msg"
-
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-try:
-    message = outlook.OpenSharedItem(current_file)
-    logger.debug('sender name: %s', message.SenderName)
-    logger.debug('sender address: %s', message.SenderEmailAddress)
-    logger.debug('sent on: %s ', message.SentOn)
-    logger.debug('sent to: %s', message.To)
-    logger.debug('CC: %s', message.CC)
-    logger.debug('BCC: %s', message.BCC)
-    logger.debug('Subject: %s', message.Subject)
-    logger.debug('Body: %s', message.Body)
+
+# current_file = dir_path + r"/example.msg"
+
+for file_name in os.listdir(dir_path):
+    if file_name.endswith(".msg"):
+
+        current_file = os.path.join(dir_path, file_name)
+        logger.debug(current_file)
+        try:
+            message = outlook.OpenSharedItem(current_file)
+            logger.debug('sender name: %s', message.SenderName)
+            logger.debug('sender address: %s', message.SenderEmailAddress)
+            logger.debug('sent on: %s ', message.SentOn)
+            logger.debug('sent to: %s', message.To)
+            logger.debug('CC: %s', message.CC)
+            logger.debug('BCC: %s', message.BCC)
+            logger.debug('Subject: %s', message.Subject)
+            logger.debug('Body: %s', message.Body)
 
 
-except AttributeError as attributeError:
-    logger.warning(attributeError)
-except Exception as this_exception:
-    logger.warning(this_exception)
+        except AttributeError as attributeError:
+            logger.warning(attributeError)
+        except Exception as this_exception:
+            logger.warning(this_exception)
 
 elapsed_time = time.time() - start_time
 logger.debug('elapsed time %d seconds', elapsed_time)
