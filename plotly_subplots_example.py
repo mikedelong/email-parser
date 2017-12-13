c480568697ba15430c211dec313980babe00c87a
Mike DeLong <mikedelong@outlook.com>
2017-12-13 08:43:02 -0500

    feature: first working version showing side-by-side subplots with no axes in common.

---
+++ plotly_subplots_example.py
@@ -0,0 +1,24 @@
+# https://plot.ly/python/subplots/
+
+from plotly import tools
+import plotly.graph_objs as graph_objs
+import plotly.offline as offline
+
+trace1 = graph_objs.Scatter(
+    x=[1, 2, 3],
+    y=[4, 5, 6]
+)
+
+trace2 = graph_objs.Scatter(
+    x=[20, 30, 40],
+    y=[50, 60, 70],
+)
+
+fig = tools.make_subplots(rows=1, cols=2)
+
+fig.append_trace(trace1, 1, 1)
+fig.append_trace(trace2, 1, 2)
+
+fig['layout'].update(height=600, width=600, title='see subplots run')
+offline.plot(fig, filename='simple-subplot')
+
