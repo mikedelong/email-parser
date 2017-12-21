3c64a5bff904e1e8fb59ec46539ddfab47c59f1c
Mike DeLong <mikedelong@outlook.com>
2017-12-21 13:53:48 -0500

    feature: empty shell with logging and overall elapsed time.

---
+++ dump-to-msg.py
@@ -0,0 +1,27 @@
+import logging
+import time
+
+import win32com.client
+
+start_time = time.time()
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logging_level = logging.INFO
+logger.setLevel(logging_level)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging_level)
+logger.debug('started')
+
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+# "6" refers to the index of a folder - in this case, the inbox.
+folder_index = 6
+inbox = outlook.GetDefaultFolder(folder_index)
+
+elapsed_time = time.time() - start_time
+logger.debug('elapsed time %d seconds', elapsed_time)
